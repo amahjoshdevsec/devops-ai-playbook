1038d7f3c3bd23daf612da898eb464372ecda7d1
amahjoshdevsec <ugochukwujoshua90@gmail.com>
2026-07-20 23:14:37 -0500

    trigger CI

--- projects/Infrastructure/main.tf
+++ projects/Infrastructure/main.tf
@@ -31,24 +31,30 @@ module "ecr" {
 }
 
 
-data "aws_eks_cluster_auth" "eks" {
-  name = module.eks.cluster_name
-}
-
 provider "kubernetes" {
   alias                  = "eks"
   host                   = module.eks.cluster_endpoint
   cluster_ca_certificate = base64decode(module.eks.cluster_certificate_authority_data)
-  token                  = data.aws_eks_cluster_auth.eks.token
+
+  exec {
+    api_version = "client.authentication.k8s.io/v1beta1"
+    command     = "aws"
+    args        = ["eks", "get-token", "--cluster-name", module.eks.cluster_name, "--region", var.region]
+  }
 }
 
 provider "helm" {
   alias = "eks"
 
-  kubernetes =  {
+  kubernetes = {
     host                   = module.eks.cluster_endpoint
     cluster_ca_certificate = base64decode(module.eks.cluster_certificate_authority_data)
-    token                  = data.aws_eks_cluster_auth.eks.token
+
+    exec = {
+      api_version = "client.authentication.k8s.io/v1beta1"
+      command     = "aws"
+      args        = ["eks", "get-token", "--cluster-name", module.eks.cluster_name, "--region", var.region]
+    }
   }
 }
 
@@ -63,4 +69,3 @@ module "argocd" {
 
   depends_on = [module.eks]
 }
-
